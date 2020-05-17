47929588178b9bb0bdd3a3bf479f6a05cc40e177
suvrajit1998 <suvrajitnaskar1998@gmail.com>
2020-05-17 20:51:02 +1300

    added redux tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,7 +12,8 @@ const store = createStore(
   rootReducer,
   linitialState,
   compose(
-    applyMiddleware(...middleware)
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
